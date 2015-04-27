d92be40670f9f877327b713b051e1d68736d1e4f
Ondrej Holecek <ondrej.holecek@firma.seznam.cz>
2015-04-27 16:36:07 +0200

    dev

--- src/main.cc
+++ src/main.cc
@@ -11,19 +11,34 @@ public:
         argc(argc),
         argv(argv),
         envp(envp),
-        label(0)
+        label(0),
+        notebook(0),
+        tabs(0),
+        window(0),
+        box(0)
     {}
+
+    void activate(GtkApplication* app);
+    
+    void open_tab();
+
 
     int argc;
     char **argv;
     char **envp;
+
+    GtkApplication* app;
     GtkWidget *label;
+    GtkWidget *notebook;
+    GtkWidget *tabs;
+    GtkWidget *window;
+    GtkWidget *box;
 };
 
 static void term_exited(GtkApplication* appx, gpointer _udata) {
-    GtkApplication* app = (GtkApplication*)_udata;
-    std::cout << app << std::endl;
-    g_application_quit(G_APPLICATION(app));
+    HisPixelApp_t *hispixel = (HisPixelApp_t *)_udata;
+    //std::cout << app << std::endl;
+    //g_application_quit(G_APPLICATION(app));
 }
 
 static gboolean keypress (GtkWidget *widget, GdkEvent *event,
@@ -32,7 +47,8 @@ static gboolean keypress (GtkWidget *widget, GdkEvent *event,
     if (event->type != GDK_KEY_PRESS) return 0;
     HisPixelApp_t *hispixel = (HisPixelApp_t *)_udata;
     if (event->key.hardware_keycode == 38) {
-        gtk_widget_hide(hispixel->label);
+        hispixel->open_tab();
+        //gtk_widget_hide(hispixel->label);
     }
 
     //if (event->key.hardware_keycode == 38) return 1;
@@ -42,32 +58,22 @@ static gboolean keypress (GtkWidget *widget, GdkEvent *event,
 
 static void activate(GtkApplication* app, gpointer _udata)
 {
-    std::cout << app << std::endl;
     HisPixelApp_t *hispixel = (HisPixelApp_t *)_udata;
-    GtkWidget *window;
+    hispixel->activate(app);
+}
 
-    window = gtk_application_window_new (app);
 
-    GdkColor color;
-    color.red = 0xffff;
-    color.green = 0;
-    color.blue = 0;
-
-    gtk_widget_modify_bg(window, GTK_STATE_NORMAL, &color);
-    gtk_window_set_title (GTK_WINDOW (window), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 400);
-
+void HisPixelApp_t::open_tab() {
     GtkWidget * terminal = vte_terminal_new();
-    g_signal_connect(terminal, "child-exited", G_CALLBACK(term_exited), app);
-    g_signal_connect(terminal, "key-press-event", G_CALLBACK(keypress), hispixel);
+    g_signal_connect(terminal, "child-exited", G_CALLBACK(term_exited), this);
+    g_signal_connect(terminal, "key-press-event", G_CALLBACK(keypress), this);
 
     GPid childpid;
-
-    const char * working_dir = 0;
-
     char *argv[2];
     argv[0] = strdup("/bin/bash");
     argv[1] = 0;
+
+    const char * working_dir = 0;
 
     int ret = vte_terminal_fork_command_full(VTE_TERMINAL(terminal),
             VTE_PTY_DEFAULT, /* VtePtyFlags pty_flags */
@@ -82,20 +88,44 @@ static void activate(GtkApplication* app, gpointer _udata)
             );
 
     ::free(argv[0]);
+    int sel = gtk_notebook_append_page(GTK_NOTEBOOK(tabs), terminal, 0);
+    std::cout << sel << std::endl;
+    gtk_notebook_set_current_page(GTK_NOTEBOOK(tabs), sel);
+    gtk_notebook_next_page (GTK_NOTEBOOK(tabs));
+
+    gtk_notebook_set_show_tabs(GTK_NOTEBOOK(tabs), 1);
+
+}
+
+void HisPixelApp_t::activate(GtkApplication* _app) {
+    app = _app;
+    window = gtk_application_window_new (app);
+
+    GdkColor color;
+    color.red = 0;
+    color.green = 0;
+    color.blue = 0;
+
+    gtk_widget_modify_bg(window, GTK_STATE_NORMAL, &color);
+    gtk_window_set_title (GTK_WINDOW (window), "Window");
+    gtk_window_set_default_size (GTK_WINDOW (window), 200, 400);
 
 
-    GtkWidget * tabs = gtk_notebook_new();
-    GtkWidget *label = gtk_label_new("smrt");
-    hispixel->label = label;
-    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 1);
+    const char * working_dir = 0;
+
+
+    tabs = gtk_notebook_new();
+    label = gtk_label_new("smrt");
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 1);
 
     gtk_box_pack_start(GTK_BOX(box), label, 0, 0, 0);
     gtk_box_pack_start(GTK_BOX(box), tabs, 1, 1, 0);
-    gtk_notebook_append_page(GTK_NOTEBOOK(tabs), terminal, 0);
-    gtk_notebook_set_show_tabs(GTK_NOTEBOOK(tabs), 0);
+    
+    open_tab();
 
     gtk_container_add (GTK_CONTAINER (window), box);
     gtk_widget_show_all (window);
+
 }
 
 int main(int argc, char **argv, char** envp)
