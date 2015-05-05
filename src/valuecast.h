aebda9f992a8ef397e3ab9021f42fd007da48bb6
Ondrej Holecek <ondrej.holecek@gmail.com>
2015-05-05 19:14:34 +0200

    polishing

--- src/valuecast.h
+++ src/valuecast.h
@@ -8,35 +8,35 @@
 #include "error.h"
 namespace s28 {
 namespace aux {
-    template<typename Type_t>
-    struct ValueCast_t {
-        static Type_t cast(const std::string &s) {
-            try {
-                return boost::lexical_cast<Type_t>(s);
-            } catch(...) {
-                RAISE(VALUE_CAST);
-            }
-            throw 1; // not reachable
-        }
-    };
-
-    template<>
-    struct ValueCast_t<bool> {
-        static bool cast(const std::string &_s) {
-            std::string s = _s;
-
-            std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-
-            if (s == "true" || s == "1" || s == "yes" ||
-                    s == "y" || s == "t") return true;
-
-            if (s == "false" || s == "0" || s == "no" ||
-                    s == "n" || s == "f") return false;
-
+template<typename Type_t>
+struct ValueCast_t {
+    static Type_t cast(const std::string &s) {
+        try {
+            return boost::lexical_cast<Type_t>(s);
+        } catch(...) {
             RAISE(VALUE_CAST);
-            throw 1; // not reachable
         }
-    };
+        throw 1; // not reachable
+    }
+};
+
+template<>
+struct ValueCast_t<bool> {
+    static bool cast(const std::string &_s) {
+        std::string s = _s;
+
+        std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+        if (s == "true" || s == "1" || s == "yes" ||
+                s == "y" || s == "t") return true;
+
+        if (s == "false" || s == "0" || s == "no" ||
+                s == "n" || s == "f") return false;
+
+        RAISE(VALUE_CAST);
+        throw 1; // not reachable
+    }
+};
 } // namespace aux
 
 template<typename Type_t>
