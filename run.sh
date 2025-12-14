773c950b17a70f6e115c9b7eea0b099d6ee6b013
Ondrej Holecek <ondrej@vivaldi.com>
2025-12-14 13:03:46 +0100

    run.sh - run with custom vte

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# to run hispixel witch curtom vte build
+
+#LD_LIBRARY_PATH=/home/smrt/w/vte/_build/src:$LD_LIBRARY_PATH ./build/src/hispixel
+LD_LIBRARY_PATH=/opt/smrt/vte/_build/src:$LD_LIBRARY_PATH /opt/smrt/hispixel/build/src/hispixel
+
